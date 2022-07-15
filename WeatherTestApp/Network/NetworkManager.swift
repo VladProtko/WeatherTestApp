cbe766d7040216dc4bb3f2a5fbbdce6b922f5d6d
Admin <ghjnmrj123@gmail.com>
2022-07-15 13:23:26 +0300

    add new branch, parsing, create func -changedegree, directionOfTheWind

--- WeatherTestApp/Network/NetworkManager.swift
+++ WeatherTestApp/Network/NetworkManager.swift
@@ -1,7 +1,25 @@
 import Foundation
 
 
- final class NetworkManager {
-
-
+final class NetworkManager {
+    
+    
+    static func loadWeather(completion: @escaping (DataWeather?, Error?)  -> Void) {
+        guard let url = URL(string: "http://api.openweathermap.org/data/2.5/forecast?lat=35&lon=139&appid=258cda358540ba669325b98f90414068") else {return}
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        
+        URLSession.shared.dataTask(with: request) { responseData, response, error in
+            
+            if let data = responseData {
+                let weather =  try? JSONDecoder().decode(DataWeather.self, from: data)
+                completion(weather, error)
+                
+            } else {
+                completion(nil,error)
+            }
+            
+        }.resume()
+    }
+    
 }
